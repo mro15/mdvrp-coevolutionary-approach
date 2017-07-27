938b422d6d43ad0a0f96eba317cb35fc0e203014
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2017-07-27 11:44:47 -0300

    inicio da main

---
+++ main.cpp
@@ -0,0 +1,7 @@
+/*  Universidade Federal do Paraná
+    Trabalho de Graduação
+    Orientador: Eduardo j. Spinosa
+    Implementação:  Lucas Fernandes de Oliveira
+                    Marcela Ribeiro de Oliveira
+    (NÃO SEI MAIS O QUE TEM QUE POR AQUI (?))
+*/

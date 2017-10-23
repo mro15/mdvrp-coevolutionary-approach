309de95601936dba31c0aca542c47d56d00e62c7
Lucas Fernandes de Oliveira <lfo14@inf.ufpr.br>
2017-10-23 12:59:49 -0200

    Atualiza script de testes
    Signed-off-by: Lucas Fernandes de Oliveira <lfo14@inf.ufpr.br>

--- scripts/tables.sh
+++ scripts/tables.sh
@@ -5,15 +5,16 @@ instances=$(ls ../instances)
 populationSize="10 50 100"
 mutationRatio="0.0625 0.125"
 iterations="10 100 1000"
+redundancy="1 2 4"
 
 for i in $instances; do
     echo '"Seed";"N° Individuals";"Mutation Ratio";"Iterations";"Duration";"Capacity";"Duration";"N° Routes";"Assignment";"Mutation Operator";"Crossover Operator";"Selection Operator";"Routes";"Graph"' > ../distances/$i.out.csv
     for p in $populationSize; do
         for it in $iterations; do
             for mr in $mutationRatio; do
-                #for j in $(seq 1 10); do
-                ../main ../instances/$i $it 10 $p $mr 1 | tail -n1 >> ../distances/$i.out.csv
-                #done
+                for red in $redundancy; do
+                    ../main ../instances/$i $it 10 $p $mr $red 5 1| tail -n1 >> ../distances/$i.out.csv
+                done
             done
         done
     done
